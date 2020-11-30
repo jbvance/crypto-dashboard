006d4a939cb07bec8d437387b08cd9b215486299
Jason Vance <jvance@NSLLP.local>
2020-11-30 12:36:29 -0600

    Begin adding search coins

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
 import ConfirmButton from './ConfirmButton';
+import Search from './Search';
 import WelcomeMessage from './WelcomeMessage';
+
 const Settings = () => {
   return (
    <Page name="settings">
       <WelcomeMessage />
       <CoinGrid topSection />
       <ConfirmButton />
+      <Search />
       <CoinGrid />
    </Page>
   )
