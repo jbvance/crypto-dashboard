2b354f6f6f3cb6efb90fdfa524c3ca327ff8d19a
Jason Vance <jvance@NSLLP.local>
2020-12-07 12:01:04 -0600

    add CoinSpotlight

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+import CoinImage from '../Shared/CoinImage';
+import { Tile } from '../Shared/Tile';
+
+const SpotlightName = styled.h2`
+  text-align: center;
+`;
+
+const CoinSpotlight = () => {
+  const appContext = useContext(AppContext);
+  const { currentFavorite, coinList } = appContext;
+  return (
+    <Tile>
+      <SpotlightName> {coinList[currentFavorite].CoinName}</SpotlightName>
+      <CoinImage spotlight coin={coinList[currentFavorite]} />
+    </Tile>
+  );
+};
+
+export default CoinSpotlight;
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import Page from '../Shared/Page';
+import CoinSpotlight from './CoinSpotlight';
 import PriceGrid from './PriceGrid';
+
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 const Dashboard = () => {
   return (
-   <Page name="dashboard">
-     <PriceGrid />
-   </Page>
-  )
+    <Page name="dashboard">
+      <PriceGrid />
+      <ChartGrid>
+        <CoinSpotlight />
+        <div>Chart goes here</div>
+      </ChartGrid>
+    </Page>
+  );
 };
 
 export default Dashboard;
